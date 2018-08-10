a7f70599f1a31254e7abf19efbe5f489207b0c85
inaronete <be.here.now1217@gmail.com>
2018-08-10 10:49:09 +0000

    validation

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -41,10 +41,12 @@ class MessagesController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'title' => 'required|max:191',
             'content' => 'required|max:191',
         ]);
         
         $message = new Message;
+        $message->title = $request->title;
         $message->content = $request->content;
         $message->save();
         
@@ -94,7 +96,15 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
+        $this->validate($request, [
+            'title' => 'required|max:191',
+            'content' => 'required|max:191',
+        ]);
+        
+        
         $message = Message::find($id);
+        $message->title = $request->title;  
         $message->content = $request->content;
         $message->save();
         
